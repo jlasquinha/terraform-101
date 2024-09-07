0f99a1c778e75b32a979063fa64d0764a2ad6b10
jlasquinha <jorgelasquinha27@gmail.com>
2024-09-07 10:54:58 -0300

    Update outputs.tf

--- terraform/main/modules/vpc/outputs.tf
+++ terraform/main/modules/vpc/outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the VPC"
-  value       = aws_vpc.giropops_vpc.id
+  value       = aws_vpc.laskaweb_de_vpc.id
 }
 
 output "vpc_cidr_block" {
   description = "The CIDR block of the VPC"
-  value       = aws_vpc.giropops_vpc.cidr_block
+  value       = aws_vpc.laskaweb_de_vpc.cidr_block
 }
 
 output "arn" {
   description = "ARN of vpc"
-  value       = aws_vpc.giropops_vpc.arn
+  value       = aws_vpc.laskaweb_de_vpc.arn
 }
